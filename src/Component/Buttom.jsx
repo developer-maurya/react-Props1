5786e1fa6bd51e5d0e661f5b0148719c47d693a7
Chitranjan kumar <chitranjan75075@gmail.com>
2025-12-17 23:59:31 +0530

    Remove Buttom component implementation

--- src/Component/Buttom.jsx
+++ src/Component/Buttom.jsx
@@ -1,14 +1,1 @@
-import React from 'react'
 
-const Buttom = (props) => {
-  return (
-    <div>
-    {props.children}
-    <button onClick={props.increment}>
-        {props.text}
-    </button>
-    </div>
-  )
-}
-
-export default Buttom
